d58b3067f67fc01ff86bc4f97701930f4f00fe2e
iorsini <isadora.o.barradas@gmail.com>
2025-04-27 18:33:34 +0100

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -29,7 +29,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (!sub)
 		return (NULL);
 	i = 0;
-	while (i < len && s[start + i])
+	while (i < len && s[start + i] != '\0')
 	{
 		sub[i] = s[start + i];
 		i++;
